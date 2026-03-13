afe644381dd9ae48511ea42d3d65339895065f97
Emre <emre.asik201010@gmail.com>
2026-03-13 14:58:38 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Ein minimaler Service Worker, um die PWA-Installations-Anforderung von Chrome zu erfüllen
+self.addEventListener('fetch', function(event) {
+    // Chrome verlangt einen Fetch-Event-Listener für PWAs
+});
